83b16d16b301edaa16d912876c644d949fff3e93
Esten Rye <esten@ryezone.net>
2018-08-31 17:05:31 -0500

    move sshpass install after ansible.

--- scripts/provision_acs.sh
+++ scripts/provision_acs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get update -y
-apt-get install software-properties-common git sshpass -y
+apt-get install software-properties-common git -y
 apt-add-repository ppa:ansible/ansible -y
 apt-get update -y
-apt-get install ansible -y
+apt-get install ansible sshpass -y
 
 git clone https://github.com/estenrye/ansible-rancher-lab.git
 chown -R vagrant ansible-rancher-lab
