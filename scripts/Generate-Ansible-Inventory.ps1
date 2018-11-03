df56f0990c750feeacf062774d6edd79f615ecc7
Esten Rye <esten@ryezone.net>
2018-11-02 21:44:07 -0500

    fix file paths.

--- scripts/Generate-Ansible-Inventory.ps1
+++ scripts/Generate-Ansible-Inventory.ps1
@@ -17,25 +17,25 @@ param(
     }
 )
 
-'' | Out-File -Force -FilePath .\inventory -Encoding utf8
+'' | Out-File -Force -FilePath $PSScriptRoot\inventory -Encoding utf8
 
 $activeVMs = Get-VM | Where-Object { !$excludeVMs.Contains($_.Name) }
 
 $activeVMs | ForEach-Object {
-    "$($_.Name) ansible_ssh_host=$($_.NetworkAdapters[0].IPAddresses[0])" | Out-File -Append -FilePath .\inventory -Encoding utf8
+    "$($_.Name) ansible_ssh_host=$($_.NetworkAdapters[0].IPAddresses[0])" | Out-File -Append -FilePath $PSScriptRoot\inventory -Encoding utf8
 }
 
-"`n[datacenter]" | Out-File -Append -FilePath .\inventory -Encoding utf8
-$activeVMs.Name | Out-File -Append -FilePath .\inventory -Encoding utf8
+"`n[datacenter]" | Out-File -Append -FilePath $PSScriptRoot\inventory -Encoding utf8
+$activeVMs.Name | Out-File -Append -FilePath $PSScriptRoot\inventory -Encoding utf8
 
 $groups.Keys | ForEach-Object {
     $machines = $groups.$_ | Where-Object { $activeVMs.Name.Contains($_) }
     if ($machines.Count -gt 0)
     {
-        "`n[$_]" | Out-File -Append -FilePath .\inventory -Encoding utf8
-        $machines | Out-File -Append -FilePath .\inventory -Encoding utf8
+        "`n[$_]" | Out-File -Append -FilePath $PSScriptRoot\inventory -Encoding utf8
+        $machines | Out-File -Append -FilePath $PSScriptRoot\inventory -Encoding utf8
     }
 }
 
 $acsIP = (Get-VM ansible-control-server).NetworkAdapters[0].IPAddresses[0]
-scp -i "$PSScriptRoot\..\.vagrant\machines\acs\hyperv\private_key" "$PSScriptRoot\inventory" vagrant@$($acsIP):~/ansible-rancher-lab
+scp -i "$PSScriptRoot\..\.vagrant\machines\acs\hyperv\private_key" "$PSScriptRoot\inventory" vagrant@$($acsIP):~/ansible-rancher-lab/inventory
